0427d7fee614c5e436a7ff485eb38602c7cfcf35
xyh123321 <376564838@qq.com>
2020-08-27 23:06:08 +0800

    [rank][jayden]test:test case2: throw new error

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -5,3 +5,12 @@ employeeTest('test case1: toString',t => {
     const employee =new Employee("jayden","engineer");
     t.is("jayden (engineer)",employee.toString())
 })
+
+employeeTest('test case2: throw new error',t => {
+    try{
+        new Employee("jayden","dev");
+        t.fail();
+    }catch(e){
+        t.is('Employee cannot be of type dev',e.message)
+    }
+})
