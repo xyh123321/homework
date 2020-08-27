d6aea720558a433ae9b6d3d5d1f3012988568e23
xyh123321 <376564838@qq.com>
2020-08-27 19:14:16 +0800

    [rank][jayden]test:add test rank case2

--- test/rankTest.js
+++ test/rankTest.js
@@ -9,7 +9,7 @@ const {rating} = require('../src/rank')
 //   t.is(await bar, 'bar');
 // });
 
-rankTest('rank case1:voyage has a zone and history has a zone', t => {
+rankTest('rank case1:voyage length is 10 and history has a zone', t => {
   const voyage = {
     zone: 'west-indies',
     length: 10,
@@ -24,4 +24,25 @@ rankTest('rank case1:voyage has a zone and history has a zone', t => {
   t.is(res,'B')
 });
 
+rankTest('rank case2:voyage length is 10 and history has not zone', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+  const history = [];
+  const res = rating(voyage, history);
+  t.is(res,'B')
+});
+
+rankTest('rank case3:voyage has not zone and history has a zone', t => {
+  const voyage = {};
+  const history = [
+    {
+      zone: 'west-indies',
+      profit: 15,
+    }
+  ];
+  const res = rating(voyage, history);
+  t.is(res,'B')
+});
 
