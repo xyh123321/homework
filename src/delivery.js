31bb322672a27218faf860569128a421bfb1bf9e
xyh123321 <376564838@qq.com>
2020-08-28 11:24:50 +0800

    [delivery][jayden]refactor: extract methods switchRushIsFalse and switchRushIsTrue

--- src/delivery.js
+++ src/delivery.js
@@ -1,32 +1,35 @@
 function deliveryDate(anOrder, isRush) {
-  if (isRush) {
-    let deliveryTime;
-    switch (anOrder.deliveryState) {
-      case 'MA' || 'CT':
-        deliveryTime = 1
-        break;
-      case 'NY' || 'NH':
-        deliveryTime = 2
-        break;
-      default:
-        deliveryTime = 3
-    }
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
+  return isRush ? switchRushIsTrue(anOrder) : switchRushIsFalse(anOrder);
+}
+
+function switchRushIsFalse(anOrder) {
+  let deliveryTime;
+  switch (anOrder.deliveryState) {
+    case 'MA' || 'CT' || 'NY':
+      deliveryTime = 2;
+      break;
+    case 'ME' || 'NH':
+      deliveryTime = 3;
+      break;
+    default:
+      deliveryTime = 4;
   }
-  else {
-    let deliveryTime;
-    switch (anOrder.deliveryState) {
-      case 'MA' || 'CT' || 'NY':
-        deliveryTime = 2
-        break;
-      case 'ME' || 'NH':
-        deliveryTime = 3
-        break;
-      default:
-        deliveryTime = 4
-    }
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
+  return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
+
+function switchRushIsTrue(anOrder) {
+  let deliveryTime;
+  switch (anOrder.deliveryState) {
+    case 'MA' || 'CT':
+      deliveryTime = 1;
+      break;
+    case 'NY' || 'NH':
+      deliveryTime = 2;
+      break;
+    default:
+      deliveryTime = 3;
   }
+  return anOrder.placedOn.plusDays(1 + deliveryTime);
 }
 
 module.exports = {
